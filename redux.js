c3c88b49c1ca99810b77e4ea8ebdb6fc9b29261e
Manish <chandrajob365@gmail.com>
2017-09-01 07:40:07 +0530

    [redux.js] Add Note app component

---
+++ redux.js
@@ -0,0 +1,239 @@
+// Store creation start //
+const validateAction = (action) => {
+  if (!action || typeof action !== 'object' || Array.isArray(action)) {
+    throw new Error('Action must be an object')
+  }
+  if (typeof action.type === 'undefined') {
+    throw new Error('Action must have a type')
+  }
+}
+
+const createStore = (reducer) => {
+  let state
+  const subscribers = []
+  const store = {
+    dispatch: (action) => { // Is dispatch a pure function , as its accession global state variable ?
+      validateAction(action)
+      console.log('{dispatch} before call to reducer state = ', JSON.stringify(state, null, 2))
+      state = reducer(state, action)
+      console.log('{dispatch} after call to reducer state = ', JSON.stringify(state, null, 2), '\n-----\n')
+      subscribers.forEach((handler) => {
+        console.log('{dispatch} handler = ', handler)
+        handler()
+      })
+    },
+    subscribe: (handler) => {
+      subscribers.push(handler)
+      return () => {
+        const index = subscribers.indexOf(handler)
+        if (index > 0) {
+          subscribers.splice(index, 1)
+        }
+      }
+    },
+    getState: () => state
+  }
+  store.dispatch({type: '@@redux/INIT'})
+  return store
+}
+// Store End //
+
+// Action Types Start//
+const CREATE_NOTE = 'CREATE_NOTE'
+const UPDATE_NOTE = 'UPDATE_NOTE'
+const OPEN_NOTE = 'OPEN_NOTE'
+const CLOSE_NOTE = 'CLOSE_NOTE'
+// Action Types End //
+
+// Reducer start //
+
+const initialState = {
+  nextNodeId: 1,
+  notes: {},
+  openNoteId: null
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_NOTE: {
+      const id = state.nextNodeId
+      const newNote = {
+        id,
+        content: ''
+      }
+      return {
+        ...state,
+        nextNodeId: id + 1,
+        openNoteId: id,
+        notes: {
+          ...state.notes,
+          [id]: newNote
+        }
+      }
+    }
+    case UPDATE_NOTE: {
+      const {id, content} = action
+      const editedNote = {
+        ...state.notes[id],
+        content
+      }
+      return {
+        ...state,
+        notes: {
+          ...state.notes,
+          [id]: editedNote
+        }
+      }
+    }
+    case OPEN_NOTE: {
+      return {
+        ...state,
+        openNoteId: action.id
+      }
+    }
+    case CLOSE_NOTE: {
+      return {
+        ...state,
+        openNoteId: null
+      }
+    }
+    default:
+      return state
+  }
+}
+// Reducer End //
+
+// Store //
+const store = createStore(reducer)
+// Store //
+
+// [Components] //
+const NoteEditor = ({note, onChangeNote, onCloseNote}) => (
+  <div>
+    <div>
+      <textarea
+        className='editor-content'
+        autoFocus
+        value={note.content}
+        onChange={event => onChangeNote(note.id, event.target.value)}
+        rows={10} cols={80}
+        />
+    </div>
+    <button className='editor-button' onClick={onCloseNote}>Close</button>
+  </div>
+)
+
+const NoteTitle = ({note}) => {
+  const title = note.content.split('\n')[0].replace(/^\s+|\s+$/g, '')
+  if (title === '') {
+    return <i>Untitled</i>
+  }
+  return <span>{title}</span>
+}
+
+const NoteLink = ({note, onOpenNote}) => (
+  <li className='note-list-item'>
+    <a href='#' onClick={() => onOpenNote(note.id)}>
+      <NoteTitle note={note} />
+    </a>
+  </li>
+)
+
+const NoteList = ({notes, onOpenNote}) => (
+  <ul className='note-list'>
+    {
+      Object.keys(notes).map(id =>
+        <NoteLink
+          key={id}
+          note={notes[id]}
+          onOpenNote={onOpenNote}
+        />
+      )
+    }
+  </ul>
+)
+
+const NoteApp = ({
+  notes, openNoteId, onAddNote, onChangeNote, onOpenNote, onCloseNote
+}) => (
+  <div>
+    {
+      openNoteId
+        ? <NoteEditor
+          note={notes[openNoteId]}
+          onChangeNote={onChangeNote}
+          onCloseNote={onCloseNote}
+        />
+        : <div>
+            <NoteList notes={notes} onOpenNote={onOpenNote} />
+            <button className='editor-button' onClick={onAddNote}>New Note</button>
+          </div>
+    }
+  </div>
+)
+
+// App Container //
+class NoteAppContainer extends React.Component {
+  constructor (props) {
+    super()
+    this.state = props.store.getState()
+    this.onAddNote = this.onAddNote.bind(this)
+    this.onChangeNote = this.onChangeNote.bind(this)
+    this.onOpenNote = this.onOpenNote.bind(this)
+    this.onCloseNote = this.onCloseNote.bind(this)
+  }
+
+  componentWillMount () {
+    this.unsubscribe = this.props.store.subscribe(() =>
+      this.setState(this.props.store.getState())
+    )
+  }
+
+  componentWillUnmount () {
+    this.unsubscribe()
+  }
+
+  onAddNote () {
+    this.props.store.dispatch({
+      type: CREATE_NOTE
+    })
+  }
+
+  onChangeNote (id, content) {
+    this.props.store.dispatch({
+      type: UPDATE_NOTE,
+      id,
+      content
+    })
+  }
+
+  onOpenNote (id) {
+    this.props.store.dispatch({
+      type: OPEN_NOTE,
+      id
+    })
+  }
+
+  onCloseNote () {
+    this.props.store.dispatch({
+      type: CLOSE_NOTE
+    })
+  }
+
+  render () {
+    return (
+      <NoteApp
+        {...this.state}
+        onAddNote={this.onAddNote}
+        onChangeNote={this.onChangeNote}
+        onOpenNote={this.onOpenNote}
+        onCloseNote={this.onCloseNote}
+      />
+    )
+  }
+}
+
+ReactDOM.render(
+  <NoteAppContainer store={store} />,
+  document.getElementById('root')
+)
